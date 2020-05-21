6e0cfc3a668b062cf3ef5bea5e13332fc8856f4a
Clement Rugwiro <41169767+crugwiro@users.noreply.github.com>
2020-05-20 20:56:34 -0700

    Delete yelp.js

--- src/Api/yelp.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.yelp.com/v3/businesses',
-    headers: {
-        Authorization: 'Bearer 5pRJLcGNp-vqPptUYbMaC0eEYjjq273WuJjGgsm0ApHaY2suC4vZYZ-he1F3Ym20mxt-O43uWHxGADfSFc3HIoeeOn5CLB48V8oPnGw96ubfbT8J3wzudsApT-qkXnYx'
-    }
-});
